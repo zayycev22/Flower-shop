7ce7f0789d8b89b95c6468160d464de3e342c24a
Yuri <zayycev22@gmail.com>
2022-03-01 01:49:25 +0300

    ссылки 2

---
+++ Shop/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from Shop.views import *
+urlpatterns = [
+    path('', index),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
